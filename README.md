b82299ef1c757813bba929882f7ea9e554b60ba8
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2022-01-15 19:44:32 +0100

    add multi core support

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Nickwasuseds-Combo-Generator
 
-Coded in python 3.6
+Coded in python 3.9
 
 # How to Run?
 ## Install Dependencies
 ``` pip3 install -r requirements.txt ```
 (requires python 3)
+
 ## Examples
-```python3 generate.py -o output2.txt -t combo``` <br>
-```python3 generate.py -o output.txt -t email```
+The Script generates a set amount of Combos using all cores.<br>
+E.g. If you have a 4 core CPU and you want 4.000.000 Combos then every core generates 1.000.000<br>
+```python3 generate.py -o output.txt -c 4000000``` <br>
+<br>
+The default values are: "output.txt", 1000000
+
+
--- generate.py
+++ generate.py
@@ -1,79 +1,78 @@
-import argparse
+import string
+import random
+import os
+import random
+import json
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--output", help="the output file", default='output.txt')
-parser.add_argument("-t", "--type", help="combo / email", default='combo')
-args = parser.parse_args()
+from tqdm.contrib.concurrent import process_map
+import multiprocessing
+
+random.seed = (os.urandom(2048))
 
 namesfile = 'names.json'
 
-combos = []
-
-
-def subcombo():
-    name = random.choice(names).lower() + random.choice(surnames).lower()
-    name_extra = ''.join(random.choice(string.digits) for _ in range(random.randint(1, 4)))
-    email_domain = random.choice(email_domains) + random.choice(domain_end)
-    email = name + name_extra + '@' + email_domain
-    password = random.choice(passwordlist)
-    combo = ('{0}:{1}'.format(email, password)).encode()
-    combos.append(combo)
-
-
-def submail():
-    name = random.choice(names).lower() + random.choice(surnames).lower()
-    name_extra = ''.join(random.choice(string.digits) for _ in range(random.randint(1, 4)))
-    email_domain = random.choice(email_domains) + random.choice(domain_end)
-    email = name + name_extra + '@' + email_domain
-    combo = ('{0}'.format(email)).encode()
-    combo = combo + '\n'.encode()
-    combos.append(combo)
-
-
-def main():
-    from tqdm import tqdm
-    for _ in tqdm(range(1000000), unit=' Combos'):
-        generator()
-    writefile()
-
-
-def writefile():
-    with open(file, 'ab') as f:
-        f.writelines(combos)
-    f.close()
-    del combos[:]
-
-
-if __name__ == "__main__":
-    file = args.output
-    try:
-        num_lines = sum(1 for _ in open(namesfile))
-        print('Got {} Names!'.format(num_lines))
-        import string
-
-        chars = string.ascii_letters + string.digits + '!@#$%^&*()'
-        import os
-        import random
-
-        random.seed = (os.urandom(2048))
-        email_domains = ['gmail.', 'yahoo.', 'hotmail.', 'aol.', 'outlook.', 'gmx.', 'Yandex.', 'icloud', 'tutanota',
+email_domains = ['gmail.', 'yahoo.', 'hotmail.', 'aol.', 'outlook.', 'gmx.', 'Yandex.', 'icloud', 'tutanota',
                          'protonmail.', 'fastmail.', 'zoho.', 'mail.', 'hushmail.', 'aichi.', 'aim.', 'airforce.',
                          'airforceemail.', 'airmail.', 'airpost.']
 
-        domain_end = ['com', 'org', 'net', 'int', 'edu', 'gov', 'ac', 'ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'an',
-                      'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb', 'bd',
-                      'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bl', 'bm', 'bn', 'bn', 'bm', 'bo', 'bq', 'br', 'bs', 'bt',
-                      'bw', 'by', 'bz', 'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl',
-                      'cm', 'cn', 'co', 'cr', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'fi', 'fj', 'fk', 'fm', 'fo', 'fr',
-                      'ga', 'gd', 'ge', 'gf', 'gg', 'gh', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu',
-                      'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'ee', 'eg', 'er', 'es', 'et', 'eu', 'id', 'ie', 'il',
-                      'im', 'in', 'io', 'iq', 'is', 'it', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh',
-                      'ki', 'km', 'kn', 'kp', 'kr', 'li', 'ma', 'mc', 'md', 'mh', 'nl', 'no', 'np', 'nz', 'om', 'pa',
-                      'pl', 'rs', 'ru', 'sa', 'sd', 'se', 'sg', 'si', 'sk', 'tc', 'td', 'tr',
-                      'tw', 'ua', 'ug', 'uk', 'um', 'us', 'uz', 'uy', 'vn', 'vu', 'za', 'zm', 'zw', 'mail']
+domain_end = ['com', 'org', 'net', 'int', 'edu', 'gov', 'ac', 'ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'an',
+                'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb', 'bd',
+                'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bl', 'bm', 'bn', 'bn', 'bm', 'bo', 'bq', 'br', 'bs', 'bt',
+                'bw', 'by', 'bz', 'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl',
+                'cm', 'cn', 'co', 'cr', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'fi', 'fj', 'fk', 'fm', 'fo', 'fr',
+                'ga', 'gd', 'ge', 'gf', 'gg', 'gh', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu',
+                'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'ee', 'eg', 'er', 'es', 'et', 'eu', 'id', 'ie', 'il',
+                'im', 'in', 'io', 'iq', 'is', 'it', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh',
+                'ki', 'km', 'kn', 'kp', 'kr', 'li', 'ma', 'mc', 'md', 'mh', 'nl', 'no', 'np', 'nz', 'om', 'pa',
+                'pl', 'rs', 'ru', 'sa', 'sd', 'se', 'sg', 'si', 'sk', 'tc', 'td', 'tr',
+                'tw', 'ua', 'ug', 'uk', 'um', 'us', 'uz', 'uy', 'vn', 'vu', 'za', 'zm', 'zw', 'mail']
 
-        import json
+combos = []
 
+class Generator:
+    def __init__(self, names, surnames, passwordlist, file, count):
+        self.names = names
+        self.surnames = surnames
+        self.passwordlist = passwordlist
+        self.file = file
+        self.count = count
+        self.combos = []
+
+    def run(self):
+        output = process_map(self.combo, range(0, multiprocessing.cpu_count()), max_workers=multiprocessing.cpu_count(), chunksize=1)
+        output = [ent for sublist in output for ent in sublist]
+        self.combos = output
+
+    def combo(self, x):
+        # This Number can max out a Ryzen 1 1600 using all cores with 12.000.000 Combos (-c 12000000) in under 15 Seconds
+        combos = []
+        for x in range(round(self.count / multiprocessing.cpu_count())):
+            name = random.choice(self.names).lower() + random.choice(self.surnames).lower()
+            name_extra = ''.join(random.choice(string.digits) for _ in range(random.randint(1, 4)))
+            email_domain = random.choice(email_domains) + random.choice(domain_end)
+            email = name + name_extra + '@' + email_domain
+            password = random.choice(self.passwordlist)
+            combo = ('{0}:{1}\n'.format(email, password)).encode()
+            combos.append(combo)
+        return combos
+
+    def write(self):
+        with open(self.file, 'ab') as f:
+            f.writelines(self.combos)
+        f.close()
+        del self.combos[:]
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", help="the output file", default='output.txt')
+    parser.add_argument("-c", "--count", help="the amount of combos", default=1000000)
+    args = parser.parse_args()
+    file = args.output
+    count = args.count
+
+    try:
         names = json.loads(open(namesfile, encoding="utf8").read())
         surnames = json.loads(open(namesfile, encoding="utf8").read())
         passwordlist = json.loads(open('passlist.json', encoding="utf8").read())
@@ -84,11 +83,6 @@ if __name__ == "__main__":
 
     print('avalible modes: standard(email:pass), email(email)')
 
-    if args.type == 'email':
-        while 1:
-            generator = submail
-            main()
-    else:
-        while 1:
-            generator = subcombo
-            main()
+    generator = Generator(names, surnames, passwordlist, file, int(count))
+    generator.run()
+    generator.write()
